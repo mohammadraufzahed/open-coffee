cd341f44d8407ade54acb68056de56a19bbd56a8
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-06-09 21:32:25 -0700

    Added the color options

--- src/Svgs/OutdoorSVG.tsx
+++ src/Svgs/OutdoorSVG.tsx
@@ -3,13 +3,14 @@ import Svg, {Path} from 'react-native-svg';
 
 type props = {
   style?: object;
+  color?: string;
 };
 
-const OutdoorSVG: React.FC<props> = ({style}) => (
+const OutdoorSVG: React.FC<props> = ({style, color}) => (
   <Svg width={13} height={12} fill="none" style={style}>
     <Path
       d="M1 11h11.5M10.79 10.56V8.33M10.8 5c-.61 0-1.1.49-1.1 1.1v1.135c0 .61.49 1.1 1.1 1.1.61 0 1.1-.49 1.1-1.1V6.1c0-.61-.49-1.1-1.1-1.1ZM6.776 10.56V6.926M6.792 1.5C5.798 1.5 5 2.298 5 3.292v1.85c0 .994.798 1.792 1.792 1.792s1.793-.798 1.793-1.792v-1.85c0-.994-.799-1.792-1.793-1.792ZM2.59 10.56V8.33M2.6 5c-.61 0-1.1.49-1.1 1.1v1.135c0 .61.49 1.1 1.1 1.1.61 0 1.1-.49 1.1-1.1V6.1c0-.61-.49-1.1-1.1-1.1Z"
-      stroke="#FFC082"
+      stroke={color}
       strokeWidth={0.8}
       strokeMiterlimit={10}
       strokeLinecap="round"
@@ -17,4 +18,7 @@ const OutdoorSVG: React.FC<props> = ({style}) => (
     />
   </Svg>
 );
+
+OutdoorSVG.defaultProps = {color: '#FFC082'};
+
 export default OutdoorSVG;
--- src/Svgs/SmokeSVG.tsx
+++ src/Svgs/SmokeSVG.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Svg, {Path} from 'react-native-svg';
 type props = {
   style?: object;
+  color?: string;
 };
-const SmokeSVG: React.FC<props> = ({style}) => (
+const SmokeSVG: React.FC<props> = ({style, color}) => (
   <Svg width={12} height={12} fill="none" style={style}>
     <Path
       d="M.375 5.625v2.813h8.063V5.624H.374Zm7.313 2.063H1.125V6.375h6.563v1.313ZM9 5.624h.75v2.813H9V5.624Zm1.313 0h.75v2.813h-.75V5.624Zm-1.28-1.563a.75.75 0 0 1 .055-.9 1.494 1.494 0 0 0 .081-1.845l-.137-.192h-.857l-.043.03.427.598a.745.745 0 0 1-.04.92 1.502 1.502 0 0 0-.11 1.805l.14.21h.902l-.418-.626Zm1.688 0a.75.75 0 0 1 .054-.9 1.494 1.494 0 0 0 .082-1.845l-.137-.192h-.857l-.043.03.427.598a.745.745 0 0 1-.041.92 1.502 1.502 0 0 0-.11 1.805l.14.21h.902l-.417-.626Z"
-      fill="#98A3E6"
+      fill={color}
     />
   </Svg>
 );
+
+SmokeSVG.defaultProps = {color: '#98A3E6'};
 
 export default SmokeSVG;
